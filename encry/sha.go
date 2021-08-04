b08c139e32a8914918ac0fa5f88f251e33f3dfdf
蚂蚱双眼皮 <429281869@qq.com>
2021-08-05 00:08:38 +0800

    Update sha.go

--- encry/sha.go
+++ encry/sha.go
@@ -20,7 +20,7 @@ func HmacSha1(key string, data string) string {
 //随机字符串
 func RandString(len int) string {
 	bytes := make([]byte, len)
-	r := rand.New(rand.NewSource(time.Now().Unix()))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < len; i++ {
 		b := r.Intn(26) + 65
 		bytes[i] = byte(b)
